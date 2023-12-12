3916b704d2c36b8a35caf7c4c82cefa76c424e07
Keith Walsh <kwalsh@redhat.com>
2023-12-12 12:36:08 -0500

    Add initial postgres query/json response
    - adds `PackagePayload` struct for a packages list
    - adds `PreFilterServer#QueryPackages` which returns a `PackagePayload` after
      querying the database for package records
    - returns the results of `QueryPackages` as JSON from `/content/packages`

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/authzed/authzed-go/v1"
 	"github.com/jackc/pgx/v5"
 	"github.com/merlante/inventory-access-poc/api"
@@ -25,7 +26,11 @@ type Package struct {
 	Synced          bool   `json:"synced"`
 }
 
-func (p Package) VisitGetContentPackagesResponse(w http.ResponseWriter) error {
+type PackagePayload struct {
+	Packages []Package `json:"packages"`
+}
+
+func (p PackagePayload) VisitGetContentPackagesResponse(w http.ResponseWriter) error {
 	jsonResponse, err := json.Marshal(p)
 	if err != nil {
 		return err
@@ -45,6 +50,31 @@ type PreFilterServer struct {
 	PostgresConn  *pgx.Conn
 }
 
+func (c *PreFilterServer) QueryPackages() (PackagePayload, error) {
+	rows, err := c.PostgresConn.Query(context.Background(), "SELECT name_id, evra, synced FROM package;")
+	if err != nil {
+		return PackagePayload{}, fmt.Errorf("Failed to query packages: %w", err)
+	}
+
+	defer rows.Close()
+	ps := PackagePayload{}
+
+	for rows.Next() {
+		var p Package
+		rows.Scan(&p.NameId, &p.Evra, &p.Synced)
+		if err != nil {
+			return PackagePayload{}, fmt.Errorf("Failed to scan packages: %w", err)
+		}
+		ps.Packages = append(ps.Packages, p)
+	}
+
+	if rows.Err() != nil {
+		return PackagePayload{}, fmt.Errorf("Failed to iterate rows: %w", rows.Err())
+	}
+
+	return ps, nil
+}
+
 func (c *PreFilterServer) GetContentPackages(ctx context.Context, request api.GetContentPackagesRequestObject) (api.GetContentPackagesResponseObject, error) {
 	ctx, span := c.Tracer.Start(ctx, "GetContentPackages")
 	defer span.End()
@@ -57,6 +87,6 @@ func (c *PreFilterServer) GetContentPackages(ctx context.Context, request api.Ge
 	time.Sleep(time.Second) // mimics the delay calling out to Postgres
 	pgSpan.End()
 
-	p := Package{NameId: 123, Evra: "1-2", DescriptionHash: "Testing", SummaryHash: "FooBar", AdvisoryId: 321, Synced: false}
-	return p, nil
+	packages, err := c.QueryPackages()
+	return packages, err
 }

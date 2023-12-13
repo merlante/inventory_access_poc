960af5cddaa2c4bb7ec5529ca28a491df8ef4482
Libor Pichler <lpichler@redhat.com>
2023-12-13 15:42:10 +0100

    Join hosts with packages

--- server/server.go
+++ server/server.go
@@ -7,21 +7,16 @@ import (
 	"github.com/authzed/authzed-go/v1"
 	"github.com/jackc/pgx/v5"
 	"github.com/merlante/inventory-access-poc/api"
+	"github.com/merlante/inventory-access-poc/cachecontent"
+	"github.com/pkg/errors"
 	"go.opentelemetry.io/otel/trace"
+	"gorm.io/gorm"
 	"net/http"
 	"time"
 )
 
-type Package struct {
-	Name               string `json:"name"`
-	Summary            string `json:"summary"`
-	SystemsApplicable  int    `json:"systems_applicable"`
-	SystemsInstallable int    `json:"systems_installable"`
-	SystemsInstalled   int    `json:"systems_installed"`
-}
-
 type PackagesPayload struct {
-	Data []Package `json:"data"`
+	Data []cachecontent.PackageAccountData `json:"data"`
 }
 
 func (p PackagesPayload) VisitGetContentPackagesResponse(w http.ResponseWriter) error {
@@ -44,35 +39,10 @@ type PreFilterServer struct {
 	PostgresConn  *pgx.Conn
 }
 
-func (c *PreFilterServer) GetPackagesPayload() (PackagesPayload, error) {
-	q := `
-		SELECT pn.name,
-		       pn.summary,
-		       res.systems_applicable,
-		       res.systems_installable,
-		       res.systems_installed
-		FROM package_account_data res
-		JOIN package_name pn ON res.package_name_id = pn.id;
-	`
-	rows, err := c.PostgresConn.Query(context.Background(), q)
-	if err != nil {
-		return PackagesPayload{}, fmt.Errorf("Failed to query packages: %w", err)
-	}
-
-	defer rows.Close()
+func (c *PreFilterServer) GetPackagesPayload(acountData []cachecontent.PackageAccountData) (PackagesPayload, error) {
 	payload := PackagesPayload{}
-
-	for rows.Next() {
-		var p Package
-		rows.Scan(&p.Name, &p.Summary, &p.SystemsApplicable, &p.SystemsInstallable, &p.SystemsInstalled)
-		if err != nil {
-			return PackagesPayload{}, fmt.Errorf("Failed to scan packages: %w", err)
-		}
-		payload.Data = append(payload.Data, p)
-	}
-
-	if rows.Err() != nil {
-		return PackagesPayload{}, fmt.Errorf("Failed to iterate rows: %w", rows.Err())
+	for _, v := range acountData {
+		payload.Data = append(payload.Data, v)
 	}
 
 	return payload, nil
@@ -92,10 +62,19 @@ func (c *PreFilterServer) GetContentPackages(ctx context.Context, request api.Ge
 	spiceSpan.End()
 
 	_, pgSpan := c.Tracer.Start(ctx, "Postgres query")
-	time.Sleep(time.Second) // mimics the delay calling out to Postgres
+
+	account := 14
+	hostIDs := []string{"0154aafc-0773-4ab7-bd5b-d018e9a85d1b", "08f3c261-9194-40db-bc48-963078a6ac12", "09854380-3a33-4446-bdf6-9e759942d681"}
+	packageAccountData := make([]cachecontent.PackageAccountData, 0)
+	countError := packagesByHostIDs(&packageAccountData, &account, hostIDs)
+	if countError != nil {
+		return nil, countError
+	}
+
+	packages, err := c.GetPackagesPayload(packageAccountData)
+
 	pgSpan.End()
 
-	packages, err := c.GetPackagesPayload()
 	return packages, err
 }
 
@@ -105,4 +84,33 @@ func getUserFromContext(ctx context.Context) (user string, found bool) {
 	}
 
 	return
+}
+
+func packagesByHostIDs(pkgSysCounts *[]cachecontent.PackageAccountData, accID *int, hostIDs []string) error {
+	err := cachecontent.WithReadReplicaTx(func(tx *gorm.DB) error {
+		q := tx.Table("system_platform sp").
+			Select(`
+				sp.rh_account_id rh_account_id,
+				spkg.name_id package_name_id,
+				count(*) as systems_installed,
+				count(*) filter (where update_status(spkg.update_data) = 'Installable') as systems_installable,
+				count(*) filter (where update_status(spkg.update_data) != 'None') as systems_applicable
+			`).
+			Joins("JOIN system_package spkg ON sp.id = spkg.system_id AND sp.rh_account_id = spkg.rh_account_id").
+			Joins("JOIN rh_account acc ON sp.rh_account_id = acc.id").
+			Joins("JOIN inventory.hosts ih ON sp.inventory_id = ih.id").
+			Where("sp.packages_installed > 0 AND sp.stale = FALSE").
+			Where("ih.id IN ?", hostIDs).
+			Group("sp.rh_account_id, spkg.name_id").
+			Order("sp.rh_account_id, spkg.name_id")
+		if accID != nil {
+			q.Where("sp.rh_account_id = ?", *accID)
+		} else {
+			q.Where("acc.valid_package_cache = FALSE")
+		}
+
+		return q.Find(pkgSysCounts).Error
+	})
+
+	return errors.Wrap(err, "failed to get counts")
 }
